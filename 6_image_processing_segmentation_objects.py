48bc8e31a9ab8ed1ffeb782c8bac9599551b3f25
André Castro <33205006+andrecastrocode@users.noreply.github.com>
2022-03-03 23:17:39 +0000

    Added watershed to cell segmentation

--- 6_image_processing_segmentation_objects.py
+++ 6_image_processing_segmentation_objects.py
@@ -6,15 +6,15 @@ from matplotlib import pyplot as plt
 
 def segment_cells():
     # Load the image
-    img = cv2.imread("resources/cells/1.TIF")
+    input = cv2.imread("resources/cells/1.TIF")
 
     # Filter only green cells
-    img = img[:,:,1]
+    img = input[:,:,1]
 
-    thresh , img_bin = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    thresh , img_bin = cv2.threshold(img, 0, 255,cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
     # min max
-    square_shape = np.ones((4,4),np.uint8)
+    square_shape = np.ones((4,4), np.uint8)
     img_bin = cv2.morphologyEx(img_bin, cv2.MORPH_OPEN, square_shape, iterations=2)
 
     # sure background area
@@ -22,14 +22,36 @@ def segment_cells():
     
     # Finding sure foreground area
     dist_transform = cv2.distanceTransform(img_bin, cv2.DIST_L2, 5)
-    ret, sure_fg = cv2.threshold(dist_transform, 0.7*dist_transform.max(), 255, 0)
+    sure_fg = cv2.erode(img_bin, square_shape, iterations=3)
+    
+    #ret, sure_fg = cv2.threshold(dist_transform, 0.7*dist_transform.max(), 255, 0)
     
     # Finding unknown region
     sure_fg = np.uint8(sure_fg)
     unknown = cv2.subtract(sure_bg,sure_fg)
 
-    cv2.imshow("msg", sure_fg)
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(sure_fg)
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers + 1
+    markers *= 100
+    # Now, mark the region of unknown with zero
+    markers[unknown == 255] = 0
 
+    img_jet_colormap = cv2.applyColorMap(np.uint8(markers), cv2.COLORMAP_JET)
+    
+    cv2.imshow("bg", sure_bg)
+    cv2.imshow("fg", sure_fg)
+    cv2.imshow("distance", dist_transform)
+    cv2.imshow("JET colormap", img_jet_colormap)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+    watershed_markers = cv2.watershed(input, markers)
+    output = input.copy()
+    output[watershed_markers == -1] = [255,0,0]
+
+    cv2.imshow("after watershed", output)
     cv2.waitKey()
     cv2.destroyAllWindows()
 
